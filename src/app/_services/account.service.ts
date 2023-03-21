153343e42043188184f70d5b5e3360b69671b076
Elvan Hidayat <elvanhd@gmail.com>
2023-03-21 22:29:07 +0700

    set the current user at application init

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -27,6 +27,10 @@ export class AccountService {
     );
   }
 
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,7 @@ export class AppComponent implements OnInit {
 
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.http.get('https://localhost:8000/api/users').subscribe({
@@ -19,5 +21,13 @@ export class AppComponent implements OnInit {
       error: (error) => console.log(error),
       complete: () => console.log('Request has completed'),
     });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
