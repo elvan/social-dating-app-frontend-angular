a24ce920aa3caebb84612b6d0662ab62ea4acd80
Elvan Hidayat <elvanhd@gmail.com>
2023-03-27 22:57:05 +0700

    create member-detail page including its photo gallery

--- src/app/_modules/shared.module.ts
+++ src/app/_modules/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [],
   imports: [
     BsDropdownModule.forRoot(),
+    NgxGalleryModule,
+    TabsModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
   ],
-  exports: [BsDropdownModule, ToastrModule],
+  exports: [BsDropdownModule, NgxGalleryModule, TabsModule, ToastrModule],
 })
 export class SharedModule {}
