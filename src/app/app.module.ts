fa082de2e7b1fe1b97bd523ac3e784181b4639a6
Elvan Hidayat <elvanhd@gmail.com>
2023-03-27 09:47:16 +0700

    create member-related components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,24 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListsComponent,
+    MemberDetailComponent,
+    MemberListComponent,
+    MessagesComponent,
+    NavComponent,
+    RegisterComponent,
+  ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
