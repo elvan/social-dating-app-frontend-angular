c4ed76fa14f2fe7498a82bb6916a9a1860d05db9
Elvan Hidayat <elvanhd@gmail.com>
2023-03-27 21:18:35 +0700

    retrieve the list of members

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  constructor() {}
+  members: Member[] = [];
 
-  ngOnInit(): void {}
+  constructor(private memberService: MembersService) {}
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: (members) => (this.members = members),
+    });
+  }
 }
