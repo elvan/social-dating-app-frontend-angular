af1364ef0a01f8ff6f071f452c90c68c501425c0
Elvan Hidayat <elvanhd@gmail.com>
2023-03-27 10:22:09 +0700

    add a route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent },
       { path: 'members/:id', component: MemberDetailComponent },
