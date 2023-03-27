35954dc5f6f2c247382537579278ab0a99613434
Elvan Hidayat <elvanhd@gmail.com>
2023-03-27 23:27:40 +0700

    add the update functionality

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
@@ -21,7 +22,11 @@ export class MemberEditComponent implements OnInit {
   member: Member | undefined;
   user: User | null = null;
 
-  constructor(private accountService: AccountService, private memberService: MembersService) {
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: (user) => (this.user = user),
     });
@@ -35,6 +40,15 @@ export class MemberEditComponent implements OnInit {
     if (!this.user) return;
     this.memberService.getMember(this.user.username).subscribe({
       next: (member) => (this.member = member),
+    });
+  }
+
+  updateMember() {
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: (_) => {
+        this.toastr.success('Profile updated successfully');
+        this.editForm?.reset(this.member);
+      },
     });
   }
 }
