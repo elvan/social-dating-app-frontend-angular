9cda7e4cd552bbd3ef3a0bca2a1e5bc2c08c7b29
Elvan Hidayat <elvanhd@gmail.com>
2024-02-11 04:27:53 +0700

    Upgrade frontend with Bootstrap and secure API calls
    - Add Bootstrap and Bootswatch for UI themes
    - Configure SSL for local development in Angular CLI
    - Upgrade project version in package.json
    - Simplify app.component.html for initial user display
    - Implement user data fetching in app.component.ts
    - Add HttpClientModule to app.module.ts for API requests
    - Ignore SSL and package-lock.json files in .gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
