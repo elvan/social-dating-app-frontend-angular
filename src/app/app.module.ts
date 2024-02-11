530d1af0e5bd2fa0a62277d00281a0daea8b21d1
Elvan Hidayat <elvanhd@gmail.com>
2024-02-11 16:58:29 +0700

    Add authentication and UI enhancements
    - Include Font Awesome for icons
    - Implement AccountService for login and registration logic
    - Add User model for type safety
    - Create NavComponent for navigation header
    - Introduce HomeComponent with toggle for register view
    - Implement RegisterComponent for user sign-up
    - Update AppComponent to set current user from local storage
    - Refactor app component HTML to use NavComponent and HomeComponent
    - Add FormsModule and BsDropdownModule for forms and Bootstrap dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
     HttpClientModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
