5f38600503af0ac6b2458fe2178922a99905a34b
Elvan Hidayat <elvanhd@gmail.com>
2024-02-25 20:49:52 +0700

    Implement like feature and user lists
    - Add methods in MembersService for liking users and retrieving likes
    - Implement pagination in ListsComponent for liked and likedBy users
    - Update ListsComponent HTML to display user cards with pagination
    - Allow users to like others from the MemberCardComponent
    - Show notifications upon liking a user using ToastrService
    - Include ngx-timeago for dynamic timestamps in member details
    - Update User model to include knownAs and gender for better filtering
    - Extend shared module with ngx-bootstrap pagination and buttons modules

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Member } from '../_models/member';
+import { Pagination } from '../_models/pagination';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css'],
 })
-export class ListsComponent {}
+export class ListsComponent {
+  members: Member[] | undefined;
+  predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination | undefined;
+
+  constructor(private memberService: MembersService) {}
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
+        this.members = response.result;
+        this.pagination = response.pagination;
+      },
+    });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
+}
