04a4fc69fbe1e7ed7fbde854a0936193eeafc7a4
Elvan Hidayat <elvanhd@gmail.com>
2024-07-21 12:30:40 +0700

    Add account service and implement login/register
    - Create User model interface for type definition
    - Implement AccountService for authentication operations
    - Update AppComponent to handle current user state
    - Add NavComponent for navigation and login/logout functionality
    - Create HomeComponent with register toggle and user display
    - Implement RegisterComponent for user registration
    - Update app configuration to provide animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()],
 };
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [FormsModule, BsDropdownModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css',
+})
+export class NavComponent {
+  accountService = inject(AccountService);
+  model: any = {};
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
